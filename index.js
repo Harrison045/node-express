5c03657c0a3208bdd4bbcbcc92957a5b334c98bc
Felix <fnarworteyjnr@gmail.com.com>
2022-12-17 15:42:38 +0000

    first commit

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 const express = require('express');
-
+const parser = require ('body-parser')
 const path = require('path')
 
 server.use(express.static(path.join(__dirname, "public")))
 
-const servicePage = (req, res) => {
-    const servicePath= path.join(__dirname,"public", "service.html")
+// const servicePage = (req, res) => {
+//     const servicePath= path.join(__dirname,"public", "service.html")
 
-res.sendfile(servicePath)
+// res.sendfile(servicePath)
 
+// }
+
+
+// const homePage = (req, res) => {
+//     const homePath= path.join(__dirname,"public", "index.html")
+
+// res.sendfile(homePath)
+
+// }
+
+
+// const aboutPage = (req, res) => {
+//     const aboutPath= path.join(__dirname,"public", "about.html")
+
+// res.sendfile(aboutPath)
+
+// }
+
+
+// const contactPage = (req, res) => {
+//     const contactPath= path.join(__dirname,"public", "contact.html")
+
+// res.sendfile(contactPath)
+
+// }
+
+const loginPage = (req , res) => {
+    console.log(req.body);
+    
+
+    res.send('i logged in yay!!! ')
 }
 
 
-const homePage = (req, res) => {
-    const homePath= path.join(__dirname,"public", "index.html")
-
-res.sendfile(homePath)
-
-}
-
-
-const aboutPage = (req, res) => {
-    const aboutPath= path.join(__dirname,"public", "about.html")
-
-res.sendfile(aboutPath)
-
-}
-
-
-const contactPage = (req, res) => {
-    const contactPath= path.join(__dirname,"public", "contact.html")
-
-res.sendfile(contactPath)
-
-}
-
-
-const server = express();
-server.get('/contact',aboutPage);
-server.get('/about',aboutPage);
-server.post('/services',servicePage);
-server.get('/',homePage);
-
-
+// const server = express();
+// server.get('/contact',aboutPage);
+// server.get('/about',aboutPage);
+// server.post('/services',servicePage);
+// server.get('/',homePage);
+server.use(parser.urlencoded({extended:true}))
+server.post('/login', loginPage )
 server.listen(3000, 'localhost', () => {
     console.log("server is in actioon")
 });
